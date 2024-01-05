e50ed350e66312c16d11c0120e1c38e37e713cb6
YANG Xudong <yangxudong@ymatrix.cn>
2024-01-05 16:37:31 +0800

    Separate header.

---
+++ src/include/port/simd/simd-lsx.h
@@ -0,0 +1,96 @@
+#ifndef INSIDE_SIMD_H
+#error "should be included via simd.h"
+#endif
+
+/*
+ *
+ */
+#include <lsxintrin.h>
+typedef __m128i Vector8;
+typedef __m128i Vector32;
+
+#include "port/simd/simd_funcs.h"
+
+static inline void
+vector8_load(Vector8 *v, const uint8 *s)
+{
+    *v = __lsx_vld(s, 0);
+}
+
+static inline void
+vector32_load(Vector32 *v, const uint32 *s)
+{
+    *v = __lsx_vld(s, 0);
+}
+
+static inline Vector8
+vector8_broadcast(const uint8 c)
+{
+    return __lsx_vldrepl_b(&c, 0);
+}
+
+static inline Vector32
+vector32_broadcast(const uint32 c)
+{
+    return __lsx_vldrepl_w(&c, 0);
+}
+
+static inline bool
+vector8_has(const Vector8 v, const uint8 c)
+{
+    return vector8_is_highbit_set(vector8_eq(v, vector8_broadcast(c)));
+}
+
+static inline bool
+vector8_has_zero(const Vector8 v)
+{
+    return vector8_has(v, 0);
+}
+
+static inline bool
+vector8_has_le(const Vector8 v, const uint8 c)
+{
+    return vector8_has_zero(vector8_ssub(v, vector8_broadcast(c)));
+}
+
+static inline bool
+vector8_is_highbit_set(const Vector8 v)
+{
+    return __lsx_vmskgez_b(v) == 0;
+}
+
+static inline bool
+vector32_is_highbit_set(const Vector32 v)
+{
+    return vector8_is_highbit_set((Vector8) v);
+}
+
+static inline Vector8
+vector8_or(const Vector8 v1, const Vector8 v2)
+{
+    return __lsx_vor_v(v1, v2);
+}
+
+static inline Vector32
+vector32_or(const Vector32 v1, const Vector32 v2)
+{
+    return __lsx_vor_v(v1, v2);
+}
+
+static inline Vector8
+vector8_ssub(const Vector8 v1, const Vector8 v2)
+{
+    return __lsx_vssub_bu(v1, v2);
+}
+
+static inline Vector8
+vector8_eq(const Vector8 v1, const Vector8 v2)
+{
+    return __lsx_vseq_b(v1, v2);
+}
+
+static inline Vector32
+vector32_eq(const Vector32 v1, const Vector32 v2)
+{
+    return __lsx_vseq_w(v1, v2);
+}
---
+++ src/include/port/simd/simd_funcs.h
@@ -0,0 +1,114 @@
+#ifndef INSIDE_SIMD_H
+#error "should be included via simd.h"
+#endif
+
+/* load/store operations */
+
+/*
+ * Load a chunk of memory into the given vector.
+ */
+static inline void vector8_load(Vector8 *v, const uint8 *s);
+
+#ifndef USE_NO_SIMD
+/*
+ * Load a chunk of memory into the given vector.
+ */
+static inline void vector32_load(Vector32 *v, const uint32 *s);
+#endif
+
+
+/* assignment operations */
+
+/*
+ * Create a vector with all elements set to the same value.
+ */
+static inline Vector8 vector8_broadcast(const uint8 c);
+
+#ifndef USE_NO_SIMD
+/*
+ * Create a vector with all elements set to the same value.
+ */
+static inline Vector32 vector32_broadcast(const uint32 c);
+#endif
+
+
+/* element-wise comparisons to a scalar */
+
+/*
+ * Return true if any elements in the vector are equal to the given scalar.
+ */
+static inline bool vector8_has(const Vector8 v, const uint8 c);
+
+/*
+ * Convenience function equivalent to vector8_has(v, 0)
+ */
+static inline bool vector8_has_zero(const Vector8 v);
+
+/*
+ * Return true if any elements in the vector are less than or equal to the
+ * given scalar.
+ */
+static inline bool vector8_has_le(const Vector8 v, const uint8 c);
+
+/*
+ * Return true if the high bit of any element is set
+ */
+static inline bool vector8_is_highbit_set(const Vector8 v);
+
+#ifndef USE_NO_SIMD
+/*
+ * Exactly like vector8_is_highbit_set except for the input type, so it
+ * looks at each byte separately.
+ *
+ * XXX x86 uses the same underlying type for 8-bit, 16-bit, and 32-bit
+ * integer elements, but Arm does not, hence the need for a separate
+ * function. We could instead adopt the behavior of Arm's vmaxvq_u32(), i.e.
+ * check each 32-bit element, but that would require an additional mask
+ * operation on x86.
+ */
+static inline bool vector32_is_highbit_set(const Vector32 v);
+#endif
+
+
+/* arithmetic operations */
+
+/*
+ * Return the bitwise OR of the inputs
+ */
+static inline Vector8 vector8_or(const Vector8 v1, const Vector8 v2);
+
+#ifndef USE_NO_SIMD
+/*
+ * Return the bitwise OR of the inputs
+ */
+static inline Vector32 vector32_or(const Vector32 v1, const Vector32 v2);
+
+/*
+ * Return the result of subtracting the respective elements of the input
+ * vectors using saturation (i.e., if the operation would yield a value less
+ * than zero, zero is returned instead).  For more information on saturation
+ * arithmetic, see https://en.wikipedia.org/wiki/Saturation_arithmetic
+ */
+static inline Vector8 vector8_ssub(const Vector8 v1, const Vector8 v2);
+#endif
+
+
+/*
+ * comparisons between vectors
+ *
+ * Note: These return a vector rather than boolean, which is why we don't
+ * have non-SIMD implementations.
+ */
+#ifndef USE_NO_SIMD
+/*
+ * Return a vector with all bits set in each lane where the corresponding
+ * lanes in the inputs are equal.
+ */
+static inline Vector8 vector8_eq(const Vector8 v1, const Vector8 v2);
+
+/*
+ * Return a vector with all bits set in each lane where the corresponding
+ * lanes in the inputs are equal.
+ */
+static inline Vector32 vector32_eq(const Vector32 v1, const Vector32 v2);
+#endif
